6ae560cd5e54d954ee1e52aeef4d9cb90fe6a019
UbaydulloH00 <turonarsloni00@gmail.com>
2023-10-26 21:45:43 +0500

    end

---
+++ index.js
@@ -0,0 +1,117 @@
+// 1 dan n gacha sonlar cjqarish
+// let n = 15;
+// for(i = 1; i<=n; i++){
+//     console.log(i);
+// }
+
+
+// n dan 1 gacha sonlarni chiqarish
+// for (i = n ; i>= 0; i--){
+    // console.log(i);
+// }
+
+// sonlarni yigindisin chiqarish 
+// let sum = 0;
+// for(i = 1;i<=n;i++){
+//     sum += i
+// }
+
+// juft va yigindi chiqarish
+// let sum = 0;
+// for (i = 1;i<=n;i++){
+//    if (i % 2 ==0 ){
+//     console.log(i);
+//    }
+//    sum += i
+// }
+
+//  toq va yigindini chiqarish
+// let sum = 0;
+// for (i = 1;i<=n;i++){
+//     if (i % 3 ==0 ){
+//      console.log(i);
+//     }
+//     sum += i
+//  }
+
+
+// n sonni k marta chiiqarish 
+// let k = 5;
+// let n = 10;
+// for (i = 1; i<=k;i++){
+//     console.log(n);
+// }
+
+
+// a dan b gacha sonlarni chiqarish
+// let a = 1;
+// let b = 10;
+// for (i = a; i<= b;i++){
+//     console.log(i);
+// }
+
+
+// b dan a gacha sonlarni chiqarish 
+
+// let a = 1;
+// let b = 10;
+// for (i = b; i<= a;i--){
+//     console.log(i);
+// }
+
+//  for 4 masla
+
+// let p = 120;
+// let sum = 0;
+// for (i = 1; i<= 10 ; i++){
+//    sum = i *p
+//    console.log(sum);
+
+// }
+
+// for 5 masala
+
+// let p = 120;
+// let sum = 0;
+// for (i = 0.1; i <= 1 ; i +=0.1){
+//    sum = i *p
+//    console.log(sum);
+
+// }
+ 
+// for 6 chi masala
+// let p = 120;
+// let sum = 0;
+// for (i = 1; i<= 2 ; i+=0.1){
+//    sum = i *p
+//    console.log(sum);
+
+// }
+
+
+//  for 7 chi masala
+// let a = 1;
+// let b = 10;
+// let sum = 0 ;
+// for (i = a; i<=b;i++){
+//     sum +=i
+// }
+
+
+//  for 8 chi masala
+// let a = 1;
+// let b = 10;
+// let sum = 0 ;
+// for (i = a; i<=b;i++){
+//     sum *=i
+// }
+
+//  for 9 chi masala
+// let a = 1;
+// let b = 10;
+// let sum = 0;
+// for (i = a ; i<= b; i++ ){
+//     sum += i**2
+// }
+// console.log(sum);
+
